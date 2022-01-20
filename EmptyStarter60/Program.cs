1d2e842214ffe2794d02aa79f1b6254a1c25a448
Daniel Akalws <danchase100@gmail.com>
2022-01-20 14:29:15 -0700

    just did

--- EmptyStarter60/Program.cs
+++ EmptyStarter60/Program.cs
@@ -6,38 +6,38 @@ namespace Cse210Starter
     {
         static void Main(string[] args)
         {
-            int i =1;
+            int i = 1;
             int row = 0;
             int col = 0;
-            string player = "X"; 
-            List<int> slot = new List<int>(); 
+            string player = "X";
+            List<int> slot = new List<int>();
             string[,] board = new string[3, 3];
             bool gameEnd = false;
 
             for (row = 0; row < 3; row++)
+            {
+                for (col = 0; col < 3; col++)
                 {
-                    for (col = 0; col < 3; col++)
-                    {
-                        
-                        board[row,col] = $"{i}";
-                        i++;
-                    }
+
+                    board[row, col] = $"{i}";
+                    i++;
                 }
+            }
             while (gameEnd == false)
-            { 
+            {
                 Console.Clear();
                 DisplayBoard(board);
-                
-                
-            //Console.WriteLine("enter row: ");
-            
+
+
+                //Console.WriteLine("enter row: ");
+
 
 
                 Console.Write("Please enter number: ");
                 string answer = Console.ReadLine();
-            
-                
-                
+
+
+
 
                 slot = FindSlot(answer);
                 board[slot[0], slot[1]] = player;
@@ -45,10 +45,10 @@ namespace Cse210Starter
 
                 DisplayBoard(board);
 
-                if (player == board[0,0] && player == board[0,1] && player == board[0,2] || player == board[1,0] && player == board[1,1] && player == board[1,2] || player == board[2,0] && player == board[2,1] && player == board[2,2] || player == board[0,0] && player == board[1,0] && player == board[2,0] || player == board[0,1] && player == board[1,1] && player == board[2,1] ||  player == board[0,2] && player == board[1,2] && player == board[2,2] || player == board[2,2] && player == board[1,1] && player == board[0,0] || player == board[0,2] && player == board[1,1] && player == board[2,0])
+                if (player == board[0, 0] && player == board[0, 1] && player == board[0, 2] || player == board[1, 0] && player == board[1, 1] && player == board[1, 2] || player == board[2, 0] && player == board[2, 1] && player == board[2, 2] || player == board[0, 0] && player == board[1, 0] && player == board[2, 0] || player == board[0, 1] && player == board[1, 1] && player == board[2, 1] || player == board[0, 2] && player == board[1, 2] && player == board[2, 2] || player == board[2, 2] && player == board[1, 1] && player == board[0, 0] || player == board[0, 2] && player == board[1, 1] && player == board[2, 0])
                 {
-                Console.WriteLine(player +" has won the game");
-                gameEnd = true;
+                    Console.WriteLine(player + " has won the game");
+                    gameEnd = true;
                 }
                 else if (Tie(board))
                 {
@@ -57,46 +57,47 @@ namespace Cse210Starter
                 }
                 player = ChangeTurn(player);
             }
+        }
 
-        static bool Tie(string [,] board)
+        static bool Tie(string[,] board)
         {
             bool isTie = false;
             for (int r = 0; r < 3; r++)
             {
                 for (int c = 0; c < 3; c++)
                 {
-                    if (board[r,c] != "X" && board[r,c] != "O")
+                    if (board[r, c] != "X" && board[r, c] != "O")
                     {
                         isTie = true;
                         break;
                     }
-                    
+
                 }
             }
 
-           
-              return !isTie;
+
+            return !isTie;
         }
-            
-            
+
+
 
 
         static string ChangeTurn(string currentPlayer)
         {
-            if ( currentPlayer == "X")
+            if (currentPlayer == "X")
             {
                 return "O";
             }
-            else 
+            else
             {
                 return "X";
             }
 
         }
-        static List <int> FindSlot( string answer)
+        static List<int> FindSlot(string answer)
         {
-            List<int>rowcol = new List<int>();
-            if (answer =="1")
+            List<int> rowcol = new List<int>();
+            if (answer == "1")
             {
                 rowcol.Add(0);
                 rowcol.Add(0);
@@ -136,31 +137,32 @@ namespace Cse210Starter
                 rowcol.Add(2);
                 rowcol.Add(1);
             }
-            else 
+            else
             {
                 rowcol.Add(2);
                 rowcol.Add(2);
             }
             return rowcol;
 
-            
-                
+
+
         }
-            static void DisplayBoard(string[,] board)
+        static void DisplayBoard(string[,] board)
+        {
+            int counte = 0;
+            for (int row = 0; row < 3; row++)
             {
-                int counte = 0;
-                for (int row = 0; row < 3; row++)
+                Console.Write("| ");
+                for (int col = 0; col < 3; col++)
                 {
-                    Console.Write("| ");
-                    for (int col = 0; col < 3; col++)
-                    {   counte++;
-                        Console.Write($"{board[row, col]} |");
-                        // Console.Write($"{counte} | ");
-                    }
-                    Console.WriteLine();
+                    counte++;
+                    Console.Write($"{board[row, col]} |");
+                    // Console.Write($"{counte} | ");
                 }
-            
+                Console.WriteLine();
             }
-        }       
+
+        }
     }
 }
+
